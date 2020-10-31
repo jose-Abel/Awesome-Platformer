70bf212334797d90cb4a122d94b964b37e85db77
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-10-31 09:45:37 -0400

    Initial commit. Create player prefab. Add rigidbody 2d. Add animator. Add player movement script. Created animation. Created transitions.

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 5f;
+
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake()
+	{
+		myBody = GetComponent<Rigidbody2D>();
+
+		anim = GetComponent<Animator>();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		PlayerWalk();
+	}
+
+	void PlayerWalk()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		
+		if (h > 0)
+		{
+			myBody.velocity = new Vector2(speed, myBody.velocity.y);
+
+			ChangeDirection(1);
+		}
+		else if (h < 0)
+		{
+			myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+
+			ChangeDirection(-1);
+
+		} else
+		{
+			myBody.velocity = new Vector2(0f, myBody.velocity.y);
+		}
+
+		anim.SetInteger("Speed", Mathf.Abs((int)myBody.velocity.x));
+	}
+
+	void ChangeDirection(int direction)
+	{
+		Vector3 tempScale = transform.localScale;
+
+		tempScale.x = direction;
+
+		transform.localScale = tempScale;
+	}
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# Awesome Platformer
+
+Project build on the Unity Game Engine and using C# programming language where I built an scalable game called "Awesome Platformer" following the guide in the Udemy course "Unity Game Development: Create 2D and 3d Games With C#" created by Awesome Tuts.
+
+
+
+This project
+![screenshot](./app_screenshot.png)
+
+
+## Built With
+- Unity game Engine
+- C# Programming Language
+- Unity C# Libraries 
+
+## Authors
+### Author1
+- 👤GitHub: [Jose Abel Ramirez](https://github.com/jose-Abel)
+
+## Getting Started
+You can clone this code anytime and open the project with Unity.
+
+
+### Setup
+You can either copy the code with "git clone" or just do a git pull on your local environment.
+
+
+### Acknowledgments
+Appreciate the teams at Udemy and the creators of the course Awesome Tuts since taught me a lot of C# and the Unity Game Engine.
+
+
+## 📝 License
+This project is MIT licensed.
+
+
+## Show your support
+Give a ⭐️ if you like this project!
